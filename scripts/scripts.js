2b519d52b297aef92eb2ef52544ff47030dbf787
anac-lalala <anaccaicedoc@gmail.com>
2020-10-19 14:24:19 -0500

    basic nav toggle

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+var navToggle = document.querySelector(".js-principal-nav__toggle");
+var navDropdown= document.querySelector(".js-principal-nav__dropdown");
+
+function toggleNav() {
+  navDropdown.classList.toggle('hidden');
+  console.log("test");
+}
+
