259985069faba8b3d847ca45cb5a2a23c3cdf60c
anac-lalala <anaccaicedoc@gmail.com>
2020-10-19 23:47:02 -0500

    hero first settings

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,8 @@
 var navToggle = document.querySelector(".js-principal-nav__toggle");
 var navDropdown= document.querySelector(".js-principal-nav__dropdown");
 
+window.matchMedia("(max-width:768px)").matches ? navDropdown.classList.add('hidden'): navDropdown.classList.remove('hidden');
+
 function toggleNav() {
   navDropdown.classList.toggle('hidden');
   console.log("test");
